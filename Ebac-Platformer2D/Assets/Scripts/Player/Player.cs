6844ce874be5066f04c1dc77cea54c75c14aaac7
FKMA-dev <fernandokenui158@gmail.com>
2025-10-09 13:46:25 +0900

    Add jump in player

--- Ebac-Platformer2D/Assets/Scripts/Player/Player.cs
+++ Ebac-Platformer2D/Assets/Scripts/Player/Player.cs
@@ -1,14 +1,18 @@
 using System.Numerics;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Player : MonoBehaviour
 {
 
     public Rigidbody2D rig;
     public float speed;
+    public float forceJump;
+    public UnityEngine.Vector2 friction = new UnityEngine.Vector2(-0.1f, 0);
 
     void Update()
     {
+        Jump();
         Move();
     }
 
@@ -22,6 +26,24 @@ public class Player : MonoBehaviour
         {
             rig.linearVelocity = new UnityEngine.Vector2(+speed, rig.linearVelocity.y);
         }
+
+        if (rig.linearVelocity.x > 0)
+        {
+            rig.linearVelocity += friction;
+        }
+        else if(rig.linearVelocity.x < 0)
+        {
+            rig.linearVelocity -= friction;
+        }
+
+    }
+
+    public void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rig.linearVelocity = UnityEngine.Vector2.up * forceJump;  
+        }   
 
     }
 }
